ca7055668470db5779e866fa36c3dee4c2813f49
Antoine Machin <machina.54@protonmail.com>
2021-07-02 18:20:35 +0200

    Added basic command completion logger.

--- src/cogs/events/logs_events.py
+++ src/cogs/events/logs_events.py
@@ -15,6 +15,11 @@ class LogsEvents(commands.Cog):
     async def on_command(self, ctx):
         logging.info(f'{ctx.author} ({ctx.author.id}) has triggered the `{ctx.command.name}` ({ctx.command.cog_name}) command in the {ctx.channel.name} ({ctx.channel.id}) channel.')
 
+    """Basic command completion logger."""
+    @commands.Cog.listener()
+    async def on_command_completion(self, ctx):
+        logging.info(f'{ctx.author} ({ctx.author.id}) completed the `{ctx.command.name}` ({ctx.command.cog_name}) command in the {ctx.channel.name} ({ctx.channel.id}) channel.')
+
     """Basic command error logger."""
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
