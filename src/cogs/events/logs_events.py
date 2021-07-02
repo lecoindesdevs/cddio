d03efbb1bc81e5af259e95c7ea287f3929015351
Antoine Machin <machina.54@protonmail.com>
2021-07-02 18:00:16 +0200

    Moved on_ready event to logs_events.py, and added some functions descriptions.

--- src/cogs/events/logs_events.py
+++ src/cogs/events/logs_events.py
@@ -5,10 +5,17 @@ class LogsEvents(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    """Basic on_ready event which runs when bot is set."""
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logging.info(f'I\'ve logged in as {self.bot.user}.')
+
+    """Basic command logger."""
     @commands.Cog.listener()
     async def on_command(self, ctx):
         logging.info(f'{ctx.author} ({ctx.author.id}) has triggered the `{ctx.command.name}` ({ctx.command.cog_name}) command in the {ctx.channel.name} ({ctx.channel.id}) channel.')
 
+    """Basic command error logger."""
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         logging.error(f'{ctx.author} ({ctx.author.id}) has raised an error in the {ctx.channel.name} ({ctx.channel.id}) channel:\n{error}')
