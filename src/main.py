e03b7e2c3ed8a6de0ccf8d1cdd7b3bf61ce2e841
Antoine Machin <machina.54@protonmail.com>
2021-07-02 17:53:01 +0200

    Changed logging level to INFO.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os, cogs_management, logging
 
 # Sets up logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 # Gets environment data.
 token, prefix = os.environ['TOKEN'], os.environ['PREFIX']
