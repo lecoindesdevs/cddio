bea00308fcaceac3597290a11b95bb8966c8a72d
Antoine Machin <machina.54@protonmail.com>
2021-07-03 10:26:35 +0200

    Upgraded ping command.

--- src/cogs/commands/misc_commands_cog.py
+++ src/cogs/commands/misc_commands_cog.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+from datetime import datetime
+import discord
 
 class MiscCommands(commands.Cog):
     def __init__(self, bot):
@@ -7,7 +9,16 @@ class MiscCommands(commands.Cog):
     """Basic ping command to get bot's latency."""
     @commands.command()
     async def ping(self, ctx):
-        await ctx.reply(f':ping_pong: {round(self.bot.latency * 1000)}ms.')
+        latency = round(self.bot.latency * 1000)
+
+        embed = discord.Embed(title=f':ping_pong: {latency}ms.', color=ctx.author.color)
+
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+
+        embed.timestamp = datetime.utcnow()
+        embed.set_footer(text='OpenCDD - Flowerybee#2006\'s fork.')
+
+        await ctx.reply(embed=embed)
 
 def setup(bot):
     bot.add_cog(MiscCommands(bot))
