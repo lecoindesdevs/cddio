5e22048a617b3b6fbdd1c926c9b7295b073e7e6e
Antoine Machin <machina.54@protonmail.com>
2021-08-05 19:09:28 +0200

    Added clean help command.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from cogs.commands.help_command import HelpCommand
 import os, cogs_management, logging
 
 # Sets up logging
@@ -8,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 token, prefix = os.environ['TOKEN'], os.environ['PREFIX']
 
 # Creates the bot without default help command.
-bot = commands.Bot(command_prefix=prefix, help_command=None)
+bot = commands.Bot(command_prefix=prefix, help_command=HelpCommand())
 
 # Adds cogs to the bot.
 cogs_management.add_cogs(bot, './cogs')
