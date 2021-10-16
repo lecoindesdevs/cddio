b7875bb27823fcc4ba5d02fdba60a257a4abc893
gly <gabin.lefranc@gmail.com>
2021-10-16 17:10:14 +0200

    Add config

---
+++ src/config.rs
@@ -0,0 +1,21 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub token: String,
+    pub prefix: String,
+    pub permissions: u64,
+}
+
+impl Config {
+    pub fn read_file<P: AsRef<std::path::Path>>(filepath: P) -> Result<Self, String> {
+        let str_config = match std::fs::read_to_string(filepath.as_ref()) {
+            Ok(v) => v,
+            Err(e) => return Err(format!("Unable to read file {}: {}", filepath.as_ref().to_string_lossy(), e.to_string())),
+        };
+        match serde_json::from_str(&str_config) {
+            Ok(v) => Ok(v),
+            Err(e) => Err(format!("Unable to parse config.json: {}", e.to_string())),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+mod config;
+
+
+trait ResultLog {
+    type OkType;
+    fn expect_log(self, msg: &str) -> Self::OkType;
+}
+impl<T, S: AsRef<str>> ResultLog for Result<T, S> {
+    type OkType=T;
+    fn expect_log(self, msg: &str) -> T {
+        match self {
+            Ok(v) => v,
+            Err(e) => {
+                panic!("{}: {}", msg, e.as_ref());
+            }
+        } 
+    }
+}
+// impl<T, S: ToString> ResultLog for Result<T, S> {
+//     type OkType=T;
+//     fn expect_log(self, msg: &str) -> T {
+//         match self {
+//             Ok(v) => v,
+//             Err(e) => {
+//                 panic!("{}: {}", msg, e.to_string());
+//             }
+//         } 
+//     }
+// }
+
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let config = config::Config::read_file("./config.json").expect_log("Could not load the configuration file");
+    println!("{:?}", config);
 }
