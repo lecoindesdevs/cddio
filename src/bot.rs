d3bbc5c1fc3f685ea8a78df766984833d27411dc
gly <gabin.lefranc@gmail.com>
2021-10-21 01:13:44 +0200

    Add dmiddleware logics

--- src/bot.rs
+++ src/bot.rs
@@ -1,13 +1,36 @@
-use crate::event::EventListenerContainer;
-use serenity::{Client, client::bridge::gateway::GatewayIntents, framework::StandardFramework};
-use crate::{commands, config::Config};
+
+use std::sync::Arc;
+
+use futures::lock::Mutex;
+use serenity::{Client, client::bridge::gateway::GatewayIntents, framework::{StandardFramework, standard::macros::group}};
+use crate::{config::Config, middleware as mw};
 
 type Result<T> = serenity::Result<T>;
 
 
-pub struct Bot {
-    client: Client
+
+struct MiddlewareHandler {
+    pub middlewares: Vec<mw::ArcMiddleware>,
+    pub framework: StandardFramework, 
+    pub event_container: mw::EventContainer
 }
+impl MiddlewareHandler {
+    pub fn add_middleware(&mut self, middleware: impl 'static+mw::Middleware) {
+        if let Some(v) = middleware.command_group() {
+            self.framework = self.framework.group(v);
+        }
+        let mw_arc = Arc::new(Mutex::new(middleware));
+        self.event_container.add_event_listener(Arc::clone(&(mw_arc as mw::ArcEvent)));
+        self.middlewares.push(mw_arc);
+    }
+}
+
+pub struct Bot {
+    client: Client,
+    middlewares: Vec<mw::ArcMiddleware>
+}
+#[group]
+struct GTest;
 
 impl Bot {
     pub async fn new(config: &Config) -> Result<Bot> {
@@ -15,16 +38,26 @@ impl Bot {
             .configure(|c| c
                 .prefix(&config.prefix)
             );
+        let mut mwh = MiddlewareHandler {
+            middlewares: Vec::new(),
+            framework,
+            event_container: mw::EventContainer::init(),
+        };
+
         let framework = commands::set_commands(framework);
+
+        let MiddlewareHandler{middlewares,framework,event_container} = mwh;
         let client = Client::builder(&config.token)
             .framework(framework)
             .intents(GatewayIntents::all())
-            .raw_event_handler(EventListenerContainer::init())
+            .raw_event_handler(event_container)
             .await?;
         Ok(Bot{
-            client
+            client,
+            middlewares
         })
     }
+    
     pub async fn start(&mut self) -> Result<()> {
         self.client.start().await
     }
--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,2 +1,23 @@
+use serenity::framework::{StandardFramework, standard::{Command, CommandGroup}};
+use std::sync::Arc;
+use futures::lock::Mutex;
+
 mod event;
 mod commands;
+
+pub use event::EventListenerContainer as EventContainer;
+pub use event::SubRawEventHandler as SubEvent;
+pub use event::ArcEvent;
+
+pub type ArcMiddleware = Arc<Mutex<dyn Middleware>>;
+
+
+pub trait Middleware: SubEvent
+{
+    fn name(&self) -> &str;
+    fn command_group<'a>(&'a self) -> Option<&'a CommandGroup>;
+}
+
+
+mod bot_start;
+pub use bot_start::BotStart;
