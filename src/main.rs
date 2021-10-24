5a1b9b97c84ac93782484ea5ca0963227eb0e1fb
gly <gabin.lefranc@gmail.com>
2021-10-25 00:16:08 +0200

    Make tests for command parser

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,36 @@ impl<T, S: AsRef<str>> ResultLog for Result<T, S> {
         } 
     }
 }
-// impl<T, S: ToString> ResultLog for Result<T, S> {
-//     type OkType=T;
-//     fn expect_log(self, msg: &str) -> T {
-//         match self {
-//             Ok(v) => v,
-//             Err(e) => {
-//                 panic!("{}: {}", msg, e.to_string());
-//             }
-//         } 
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use crate::component;
 
+    #[test]
+    fn command() {
+        use component::command_parser as cmd;
+        let cmd = cmd::Command::new("command")
+            .set_help("Une commande de test")
+            .add_param(
+                cmd::CommandParameter::new("param")
+                    .set_help("Un paramètre")
+                    .value_type("texte")
+            );
+        use component::command_parser::{split_shell, ParseError, matching};
+        assert_eq!(cmd.try_match(split_shell(r#"command -param "Je suis un parametre" -unknown"#)), Err(ParseError::UnknownParameter("-unknown")));
+        assert_eq!(cmd.try_match(split_shell(r#"command -param"#)), Err(ParseError::MissingParameterValue("-param")));
+        assert_eq!(cmd.try_match(split_shell(r#"command -param "Je suis un parametre""#)), Ok(matching::Command{
+            name: "command",
+            params: vec![ matching::Parameter{ name: "param", value: "Je suis un parametre" } ]
+        }));
+    }
+    #[test]
+    fn split_shell() {
+        let args = component::command_parser::split_shell(r#"command -param "Je suis un parametre""#);
+        for v in args {
+            println!("{}", v);
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
