d13fb0f76a539ec340832382acba680ecf3a465d
gly <glefranc@eig.fr>
2021-10-25 16:04:46 +0200

    Add tests examples

--- src/main.rs
+++ src/main.rs
@@ -21,32 +21,68 @@ impl<T, S: AsRef<str>> ResultLog for Result<T, S> {
 }
 #[cfg(test)]
 mod tests {
-    use crate::component;
+    use crate::component::{self, command_parser::{ParseError, matching}};
 
     #[test]
-    fn command() {
+    fn command_parser() {
         use component::command_parser as cmd;
+        
         let cmd = cmd::Command::new("command")
             .set_help("Une commande de test")
             .add_param(
                 cmd::CommandParameter::new("param")
                     .set_help("Un paramètre")
-                    .value_type("texte")
+                    .set_value_type("texte")
             );
         use component::command_parser::{split_shell, ParseError, matching};
-        assert_eq!(cmd.try_match(split_shell(r#"command -param "Je suis un parametre" -unknown"#)), Err(ParseError::UnknownParameter("-unknown")));
-        assert_eq!(cmd.try_match(split_shell(r#"command -param"#)), Err(ParseError::MissingParameterValue("-param")));
-        assert_eq!(cmd.try_match(split_shell(r#"command -param "Je suis un parametre""#)), Ok(matching::Command{
-            name: "command",
+        assert_eq!(cmd.try_match(&split_shell(r#"command -param "Je suis un parametre" -unknown"#)), Err(ParseError::UnknownParameter("-unknown")));
+        assert_eq!(cmd.try_match(&split_shell(r#"command -param"#)), Err(ParseError::MissingParameterValue("-param")));
+        assert_eq!(cmd.try_match(&split_shell(r#"command -param "Je suis un parametre""#)), Ok(matching::Command{
+            path: vdq!["command"],
             params: vec![ matching::Parameter{ name: "param", value: "Je suis un parametre" } ]
+        }));
+    }
+    #[test]
+    fn group_parser() {
+        use component::command_parser as cmd;
+        let group = cmd::Group::new("group1")
+            .add_group(
+                cmd::Group::new("group2")
+                    .add_command(cmd::Command::new("command1"))
+                    .add_command(cmd::Command::new("command3")
+                        .add_param(cmd::CommandParameter::new("param"))
+                        .add_param(cmd::CommandParameter::new("param2")
+                            .set_required(true)
+                        )
+                    )
+            )
+            .add_command(cmd::Command::new("command2"));
+        use component::command_parser::split_shell;
+        assert_eq!(group.try_match(&split_shell(r#"group1 group2 command1"#)), Ok(cmd::matching::Command {
+            path: vdq!["group1", "group2", "command1"],
+            params: vec![]
+        }));
+        assert_eq!(group.try_match(&split_shell(r#"group1 group2 command3 -param "Test param" -param2 test"#)), Ok(cmd::matching::Command {
+            path: vdq!["group1", "group2", "command3"],
+            params: vec![cmd::matching::Parameter{
+                name:"param",
+                value: "Test param"
+            },
+            cmd::matching::Parameter{
+                name:"param2",
+                value: "test"
+            }]
+        }));
+        assert_eq!(group.try_match(&split_shell(r#"group1 group2 command3 -param "Test param""#)), Err(ParseError::RequiredParameters("param2".to_string())));
+        assert_eq!(group.try_match(&split_shell(r#"group1 command2"#)), Ok(cmd::matching::Command {
+            path: vdq!["group1", "command2"],
+            params: vec![]
         }));
     }
     #[test]
     fn split_shell() {
         let args = component::command_parser::split_shell(r#"command -param "Je suis un parametre""#);
-        for v in args {
-            println!("{}", v);
-        }
+        assert_eq!(args, vec!["command", "-param", "Je suis un parametre"]);
     }
 }
 
