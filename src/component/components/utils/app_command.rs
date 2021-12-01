3a15eaafcf7636827f756abd3d4a645b84561c90
gly <glefranc@eig.fr>
2021-12-01 11:15:54 +0100

    Add AppCommEmbed doc

--- src/component/components/utils/app_command.rs
+++ src/component/components/utils/app_command.rs
@@ -18,11 +18,21 @@ impl<'a> CommandType<'a> {
     }
 }
 
+/// # Conteneur d'application command
+/// 
+/// Lorsque le bot recoie une commande via un evenement d'interaction, cette structure est un utilitaire
+/// pour accéder aux données d'une struct ApplicationCommandInteraction plus facilement. La commande et 
+/// ses arguments pouvant se situer à différent niveau en fonction des sous groupes, [`get_argument`] 
+/// permet d'obtenir directement les arguments.
+/// 
+/// [`get_argument`]: `Self::get_argument`
 pub struct ApplicationCommandEmbed<'a>(pub &'a ApplicationCommandInteraction, CommandType<'a>);
 
 impl<'a> ApplicationCommandEmbed<'a> {
+    /// Créer un conteneur d'application command
+    /// 
+    /// La (sous) commande est recherchée dans la commande principale, puis dans les options.
     pub fn new(interaction: &'a ApplicationCommandInteraction) -> Self {
-        
         let mut command = CommandType::Command(&interaction.data);
         loop {
             let options = match command {
@@ -40,6 +50,9 @@ impl<'a> ApplicationCommandEmbed<'a> {
         }
         ApplicationCommandEmbed(interaction, command)
     }
+    /// Retourne le nom de la commande complète.
+    /// 
+    /// Ca inclut le nom des sous groupes et de la commande tel que `groupe.sous_groupe.commande`
     pub fn fullname(&self) -> String {
         let mut names = vec![self.0.data.name.as_str()];
         let mut cmd = self.0.data.options.first();
@@ -53,14 +66,18 @@ impl<'a> ApplicationCommandEmbed<'a> {
         }
         names.join(".")
     }
+    /// Retourne l'id du serveur sur lequel la commande a été effectuée.
     pub fn get_guild_id(&self) -> Option<GuildId> {
         self.0.guild_id
     }
+    /// Cherche et retourne l'argument `name`.
     pub fn get_argument(&'a self, name: &str) -> Option<&'a ApplicationCommandInteractionDataOption> {
         self.1.get_argument(name)
     }
 }
-
+/// Helper pour accéder à un argument d'une commande
+/// 
+/// Fait plusieurs vérifications pour obtenir l'argument et simplifie la lecture du code
 macro_rules! get_argument_result {
     ($app_command:expr, $name:expr, $typ:ident) => {
         match $app_command.get_argument($name) {
@@ -79,6 +96,9 @@ macro_rules! get_argument_result {
         }
     };
 }
+/// Helper pour accéder à un argument d'une commande s'il existe
+/// 
+/// Fait plusieurs vérifications pour obtenir l'argument et simplifie la lecture du code
 macro_rules! get_optional_argument_result {
     ($app_command:expr, $name:expr, $typ:ident) => {
         match $app_command.get_argument($name) {
@@ -97,6 +117,10 @@ macro_rules! get_optional_argument_result {
         }
     };
 }
+/// Helper pour accéder à un argument d'une commande
+/// 
+/// Contraitement à [`get_argument_result!`] et [`get_optional_argument_result!`], cette macro ne fait 
+/// pas de vérification superflu et retourne l'argument de la commande si les critères sont remplis.
 macro_rules! get_argument {
     ($app_command:expr, $name:expr, $typ:ident) => {
         match $app_command.get_argument($name) {
