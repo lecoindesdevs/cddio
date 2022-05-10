3abcfcf71b4c3bb7c6c4add5be816759aa23be04
Gly <gabin.lefranc@gmail.com>
2022-05-11 00:46:13 +0200

    Add EventDispatcher

--- deps/opencdd-components/src/event.rs
+++ deps/opencdd-components/src/event.rs
@@ -1,5 +1,28 @@
-use serenity::{model::event::Event, client::Context};
+use serenity::{model::event::Event, client::{Context, RawEventHandler}, async_trait};
 
-pub trait ComponentEvent {
+use crate::Components;
+
+pub trait ComponentEvent: Sync + Send  {
     fn event(&mut self, ctx: &Context, event: &Event);
+}
+
+struct ComponentEventDispatcher {
+    components: Components
+}
+
+impl ComponentEventDispatcher {
+    fn new(components: Components) -> Self { 
+        Self { components } 
+    }
+}
+
+
+
+#[async_trait]
+impl RawEventHandler for ComponentEventDispatcher {
+    async fn raw_event(&self, ctx: Context, ev: Event) {
+        for comp in &self.components {
+            comp.lock().await.event(&ctx, &ev)
+        }
+    }
 }
