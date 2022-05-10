da74fa783daa00afbc2bf0f46c404ee79fcfbfef
Gly <gabin.lefranc@gmail.com>
2022-05-11 00:46:31 +0200

    Add ComponentContainer

--- deps/opencdd-components/src/lib.rs
+++ deps/opencdd-components/src/lib.rs
@@ -1,7 +1,29 @@
 pub mod declarative;
 pub mod event;
+use std::sync::Arc;
+
 pub use declarative::ComponentDeclarative;
 pub use event::ComponentEvent;
+use serenity::prelude::Mutex;
 
 pub trait Component: ComponentDeclarative + ComponentEvent {}
 
+pub type Components = Vec<Arc<Mutex<dyn Component>>>;
+
+pub struct ComponentContainer(Components);
+
+impl ComponentContainer {
+    pub fn new() -> ComponentContainer {
+        ComponentContainer(Vec::new())
+    }
+}
+impl AsRef<Components> for ComponentContainer {
+    fn as_ref(&self) -> &Components {
+        &self.0
+    }
+}
+impl AsMut<Components> for ComponentContainer {
+    fn as_mut(&mut self) -> &mut Components {
+        &mut self.0
+    }
+}
