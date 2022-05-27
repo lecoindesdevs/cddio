fca360d49015d10309dfd89884fe807fda4f451b
Gly <gabin.lefranc@gmail.com>
2022-05-27 02:19:53 +0200

    Iter flat over declarative nodes

--- deps/opencdd-components/src/declarative.rs
+++ deps/opencdd-components/src/declarative.rs
@@ -1,6 +1,6 @@
-use std::sync::Arc;
+use std::{slice::Iter, fmt::Display};
 
-use serenity::{model::{id::GuildId, interactions::application_command::ApplicationCommandOptionType}, builder::{CreateApplicationCommands, CreateApplicationCommandOption, CreateApplicationCommand}, client::Context};
+use serenity::{model::{interactions::application_command::ApplicationCommandOptionType}, builder::{CreateApplicationCommands, CreateApplicationCommandOption, CreateApplicationCommand}};
 
 pub trait ComponentDeclarative{
     fn declarative(&self) -> Option<&'static Node> {
@@ -20,6 +20,9 @@ impl Node {
         for command in self.commands {
             commands.add_application_command(command.into());
         }
+    }
+    pub fn iter_flat(&'static self) -> IterFlatNode {
+        IterFlatNode::new(self)
     }
 }
 pub struct ChildNode {
@@ -105,3 +108,69 @@ impl From<&Argument> for CreateApplicationCommandOption {
     }
 }
 
+
+pub struct IterFlatNode
+{
+    // node: &'static Node,
+    // nodes_chain: Vec<IterFlatCommand>,
+    name: Option<&'static str>,
+    children: Iter<'static, ChildNode>,
+    current_child: Option<Box<IterFlatNode>>,
+    commands: Iter<'static, Command>,
+}
+
+impl IterFlatNode
+{
+    pub fn new(node: &'static Node) -> Self {
+        IterFlatNode {
+            name: None,
+            children: node.children.iter(),
+            current_child: None,
+            commands: node.commands.iter(),
+        }
+    }
+    fn from_child(node: &'static Node, name: &'static str) -> Self {
+        IterFlatNode {
+            name: Some(name),
+            children: node.children.iter(),
+            current_child: None,
+            commands: node.commands.iter(),
+        }
+    }
+}
+
+pub enum IterType {
+    Node(&'static ChildNode),
+    Command(&'static Command)
+}
+
+impl Iterator for IterFlatNode
+{
+    type Item = (String, IterType);
+    fn next(&mut self) -> Option<Self::Item> {
+        let fullname = |other: &str| {
+            match self.name {
+                Some(name) => format!("{} {}", name, other),
+                None => other.into(),
+            }
+        };
+        loop {
+            if let Some(node) = &mut self.current_child {
+                match node.next() {
+                    Some((name, v)) => return Some((fullname(&name), v)),
+                    None => {
+                        self.current_child = None;
+                        continue;
+                    }
+                }
+            } else if let Some(child_node) = self.children.next() {
+                self.current_child = Some(Box::new(IterFlatNode::from_child(&child_node.node, child_node.name)));
+                return Some((fullname(child_node.name.into()), IterType::Node(child_node)));
+            } else if let Some(command) = self.commands.next() {
+                return Some((fullname(command.name.into()), IterType::Command(command)));
+            } else {
+                return None;
+            }
+        }
+    }
+}
