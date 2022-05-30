c6c9ad6e8c5ca0221db2560e10f6cd39a07cbf27
Gly <gabin.lefranc@gmail.com>
2022-05-30 19:02:44 +0200

    Help clean code

--- src/component_system/components/help/mod.rs
+++ src/component_system/components/help/mod.rs
@@ -1,35 +1,13 @@
 //! Le composant help permet d'afficher une aide en fonction de la commande.
 //! Il se repose sur le groupe de commande retournée par la fonction [`Component::group_parser`].
 
-use cmp2::declarative::{IterType, Node};
+use cmp2::declarative::IterType;
 use opencdd_components::{self as cmp2, ApplicationCommandEmbed, message, message::ToMessage};
 use opencdd_macros::commands;
-use serenity::{async_trait, utils::Colour, client::Context, model::{interactions::{application_command::ApplicationCommandInteraction, InteractionApplicationCommandCallbackDataFlags}, event::InteractionCreateEvent}, builder::CreateEmbed};
-
-use crate::component_system::{self as cmp, command_parser::{self as cmd, Named}, manager::{ArcManager}};
-
-use super::utils::commands::*;
+use serenity::client::Context;
 
 pub struct Help {
     container: cmp2::container::RefContainer,
-}
-
-#[derive(Debug, Default)]
-struct HelpInfo {
-    /// Nom du groupe ou de la commande
-    name: String,
-    /// Description du groupe ou de la commande
-    desc: Option<String>,
-    /// Permission (role) requise pour être utilisé
-    permission: Option<String>,
-    /// Si l'aide concerne un groupe, la liste des sous-groupes, s'il y en a
-    groups: Option<Vec<(String, Option<String>)>>,
-    /// Si l'aide concerne un groupe, la liste des sous-commande, s'il y en a
-    commands: Option<Vec<(String, Option<String>)>>,
-    /// Si l'aide concerne une commande, la liste des paramètres, s'il y en a
-    params: Option<Vec<(String, Option<String>)>>,
-    /// Si aide générale, la liste des composants
-    components: Option<Vec<String>>
 }
 
 impl Help {
@@ -53,7 +31,12 @@ impl Help {
             Some((_, IterType::Node(node))) => node.to_message(),
             None => message::error("Commande inconnue"),
         };
-        app_cmd.direct_response(ctx, msg).await;
+        match app_cmd.direct_response(ctx, msg).await {
+            Err(e) => {
+                println!("{}", e);
+            }
+            _ => (),
+        }
     }
     #[command(description="Affiche la liste des commandes du bot")]
     async fn liste_commandes(&self, ctx: &Context, app_cmd: ApplicationCommandEmbed<'_>) {
@@ -70,7 +53,12 @@ impl Help {
             .map(|(fullname, iter_type)| format!("**{}**: {}", fullname, iter_type.description))
             .collect::<Vec<_>>()
             .join("\n");
-        app_cmd.direct_response(ctx, message::success(msg)).await;
+        match app_cmd.direct_response(ctx, message::success(msg)).await {
+            Err(e) => {
+                println!("{}", e);
+            }
+            _ => (),
+        }
     }
 
     async fn get_command_info(&self, command: &str) -> Option<(String, IterType)> {
