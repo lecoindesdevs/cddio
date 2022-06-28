ff5112bed5f201b5536fa0ece49ad7a3ea28f02e
Gly <gabin.lefranc@gmail.com>
2022-06-29 01:27:00 +0200

    Improve task scheduler

--- src/component_system/components/utils/task2.rs
+++ src/component_system/components/utils/task2.rs
@@ -1,11 +1,17 @@
-use std::time::Duration;
+use std::{time::Duration, collections::HashMap, sync::Arc};
 
 use chrono::{DateTime, Utc};
+use futures_locks::Mutex;
 use serde::{Deserialize, Serialize};
 use serenity::async_trait;
 
+#[async_trait]
+pub trait DataFunc: Send + Sync + 'static {
+    async fn run(&self) -> Result<(), String>;
+}
+
 #[derive(Serialize, Deserialize, Clone)]
-pub struct Task<D> 
+pub struct Task<D: DataFunc + Clone> 
 {
     until: i64,
     data: D
@@ -13,64 +19,88 @@ pub struct Task<D>
 
 pub type TaskID = u64;
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct RegistryTask<D>
+#[derive(Clone)]
+struct RegistryTask<D> where
+    D: DataFunc + Clone
 {
-    pub id: TaskID,
-    pub task: Task<D>
+    id: TaskID,
+    task: Task<D>
 }
 
 #[async_trait]
-pub trait Registry<D> {
-    async fn register(&self, task: Task<D>) -> Result<TaskID, String>;
-    async fn unregister(&self, id: TaskID) -> Result<(), String>;
-    async fn get(&self, id: TaskID) -> Option<Task<D>>;
-    async fn get_all(&self) -> Vec<RegistryTask<D>>;
+pub trait Registry
+{
+    type Data: DataFunc + Clone;
+    async fn register(&mut self, task: Task<Self::Data>) -> Result<TaskID, String>;
+    async fn unregister(&mut self, id: TaskID) -> Result<(), String>;
+    async fn get(&self, id: TaskID) -> Option<Task<Self::Data>>;
+    async fn get_all(&self) -> Vec<RegistryTask<Self::Data>>;
 }
 
+type Tasks<R: Registry> = Arc<Mutex<R>>;
+
 pub struct TaskManager<D, R> where
-    R: Registry<D>
+    D: DataFunc + Clone,
+    R: Registry<Data = D> + Send + 'static
 {
-    task_func: Box<dyn Fn(D) -> () + Send + Sync>,
-    registry: R,
+    tasks: Tasks<R>,
+    task_handles: HashMap<TaskID, tokio::task::JoinHandle<()>>
 }
 
 impl<D, R> TaskManager<D, R> where
-    R: Registry<D>
+    D: DataFunc + Clone,
+    R: Registry<Data = D> + Send + 'static
 {
-    pub fn new(task_func: impl Fn(D) -> () + Send + Sync + 'static, registry: R) -> Self {
+    pub fn new(registry: R) -> Self {
         Self {
-            task_func: Box::new(task_func),
-            registry
+            tasks: Arc::new(Mutex::new(registry)),
+            task_handles: HashMap::new()
         }
     }
-    pub async fn add(&self, data: D, until: DateTime<Utc>) -> Result<TaskID, String> {
-        self.registry.register(Task { until: until.timestamp(), data }).await
+    pub async fn add(&mut self, data: D, until: DateTime<Utc>) -> Result<TaskID, String> {
+        let mut tasks = self.tasks.lock().await;
+        let id = tasks.register(Task {
+            until: until.timestamp(),
+            data: data.clone()
+        }).await?;
+        let tasks = Arc::clone(&self.tasks);
+        let handle = tokio::spawn(async move {
+            tokio::time::sleep(Duration::from_secs((until.timestamp() - Utc::now().timestamp()) as _ )).await;
+            data.run().await;
+            Self::remove_from_registry(&tasks, id).await;
+        });
+        self.task_handles.insert(id, handle);
+        Ok(id)
     }
-    pub async fn remove(&self, id: TaskID) -> Result<(), String> {
-        self.registry.unregister(id).await
+    pub async fn remove(&mut self, id: TaskID) -> Result<(), String> {
+        match self.task_handles.get(&id){
+            Some(handle) => {
+                handle.abort();
+                self.task_handles.remove(&id);
+                Self::remove_from_registry(&self.tasks, id).await;
+                Ok(())
+            },
+            None => Err("Task not found".to_string())
+        }
+    }
+    async fn remove_from_registry(tasks: &Tasks<R>, id: TaskID) -> Result<(), String> {
+        let mut tasks = tasks.lock().await;
+        tasks.unregister(id).await;
+        Ok(())
     }
     pub async fn get(&self, id: TaskID) -> Option<Task<D>> {
-        self.registry.get(id).await
+        let tasks = self.tasks.lock().await;
+        tasks.get(id).await
     }
-    pub fn reset_func(&mut self, task_func: impl Fn(D) -> () + Send + Sync + 'static) {
-        self.task_func = Box::new(task_func);
-    }
-    pub async fn run(&self) {
-        loop {
-            let tasks = self.registry.get_all().await;
-            if tasks.len() == 0 {
-                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-                continue;
-            }
-            let now = Utc::now();
-            for RegistryTask{id: task_id, task} in tasks {
-                if task.until < now.timestamp() {
-                    self.task_func.as_ref()(task.data);
-                    self.remove(task_id).await;
-                }
-            }
-            tokio::time::sleep(Duration::from_secs(10)).await;
+}
+
+impl<D, R> Drop for TaskManager<D, R> where
+    D: DataFunc + Clone,
+    R: Registry<Data = D> + Send + 'static
+{
+    fn drop(&mut self) {
+        for (_, task) in self.task_handles.iter() {
+            task.abort();
         }
     }
 }
