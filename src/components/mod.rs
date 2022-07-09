867691267f6a03d107192865b8d1f14d92cd56f5
Gly <gabin.lefranc@gmail.com>
2022-07-09 02:42:48 +0200

    Remove old stuffs

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,6 +1,5 @@
 //! Module comportant les composants
 
-
 mod misc;
 pub use misc::*;
 mod help;
@@ -13,8 +12,6 @@ mod modo;
 pub use modo::*;
 mod autobahn;
 pub use autobahn::*;
-pub mod test_component2;
-// pub use test_component2::*;
 
 // Fonctions utiles pour les composants
 mod utils;
