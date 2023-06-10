8251fab0f96a72ecabc248f267dba72d53aa0a63
Gly <gabin.lefranc@gmail.com>
2023-06-10 17:11:47 +0200

    optimize tables creation

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,4 +1,6 @@
 pub mod discord;
+pub mod archive;
+use crate::{log_info};
 
 use sea_orm::{Database, DbConn, DbErr, Schema, ConnectionTrait, TransactionTrait};
 
@@ -8,16 +10,37 @@ pub async fn start_db(url: &str) -> Result<DbConn, DbErr> {
     Ok(db)
 }
 
+macro_rules! create_tables_if_not_exists {
+    ($transaction:ident, $schema:ident, $builder:ident, $($name:path),*) => {
+        $(
+            log_info!("Creating table {}", stringify!($name));
+            let res = $transaction.execute(
+                $builder.build($schema.create_table_from_entity($name).if_not_exists())
+            ).await?;
+            log_info!("Table {} created: {:?}", stringify!($name), res);
+        )*
+    };
+}
+
 async fn check_tables(db: &DbConn) -> Result<(), DbErr> {
     let builder = db.get_database_backend();
     let schema = Schema::new(builder);
     let transaction = db.begin().await?;
 
-    transaction.execute(builder.build(schema.create_table_from_entity(discord::User).if_not_exists())).await?;
-    transaction.execute(builder.build(schema.create_table_from_entity(discord::Channel).if_not_exists())).await?;
-    transaction.execute(builder.build(schema.create_table_from_entity(discord::Message).if_not_exists())).await?;
-    transaction.execute(builder.build(schema.create_table_from_entity(discord::Attachment).if_not_exists())).await?;
-    transaction.commit().await?;
-    
-    Ok(())
+    log_info!("Creating tables");
+
+    create_tables_if_not_exists!(transaction, schema, builder, 
+        discord::User, 
+        discord::Channel, 
+        discord::Message, 
+        discord::Attachment,
+        archive::Archive
+    );
+    match transaction.commit().await {
+        Ok(v) => {
+            log_info!("Tables created");
+            Ok(())
+        },
+        e => e,
+    }
 }
