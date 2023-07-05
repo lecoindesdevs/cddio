7dca5d2a1cc2cfed3e844500127a71c32876065b
Gly <gabin.lefranc@gmail.com>
2023-07-05 15:47:24 +0200

    Impl Display for db controller Error

--- src/db/controller/mod.rs
+++ src/db/controller/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub mod ticket;
 pub mod discord;
 
@@ -6,4 +8,24 @@ pub enum Error {
     Serenity(serenity::Error),
     SeaORM(sea_orm::DbErr),
     Custom(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Serenity(e) => {
+                f.write_str("Serenity error: ")?;
+                e.fmt(f)
+            },
+            Error::SeaORM(e) => {
+                f.write_str("sea-orm error: ")?;
+                e.fmt(f)
+            }
+            Error::Custom(e) => {
+                f.write_str("Database error: ")?;
+                e.fmt(f)
+            }
+        }
+        
+    }
 }
